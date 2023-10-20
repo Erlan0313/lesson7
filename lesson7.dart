6ee1e47287f76ca7f657c5f5e840f19f1fecd439
Erlan0313 <attokuroverlan2003@gmail.com>
2023-10-20 17:37:07 +0600

    first commit

---
+++ lesson7.dart
@@ -0,0 +1,33 @@
+void main(List<String> args) {
+ /* String name = 'Erlan';
+  print('длина строки = ${name.length} symbols');
+  String surname = 'Attokurov';
+  print('My name is name');
+  print('My name is $name, surname $surname');
+
+  int a = 10;
+  int b = 20;
+  print('a+b= $a+$b    ');
+  print('a + b = ${a*b}');
+  print('$a + $b = ${a*b}');*/
+
+  // for (var i = 0; i < 15; i++) {
+ 
+  //   print('$i Hello World');
+  // }
+  // List names = ['aa','bb','cc','dd','ee','ff','gg','hh'];
+  // for (var i = 0; i < names.length; i++) {
+  //   print(names);
+  //   print(names[i]);
+  // }
+
+
+  for (int i=20;i>=10;i--){
+    print('number $i');
+  }
+  // int a = 10;
+  // print(a+=5);
+  // print(a-=5);
+  // print(a*=5);
+  // print(a~/=5);
+}
